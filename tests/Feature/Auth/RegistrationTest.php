d17dfd25f375c42e4e592c9e676da71a22c2cdd0
Jordan Partridge <jordan@partridge.rocks>
2025-12-04 23:10:45 -0700

    Add describe blocks to AuthenticationTest and RegistrationTest

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,19 +1,21 @@
 <?php
 
-test('registration screen can be rendered', function () {
-    $response = $this->get(route('register'));
+describe('registration', function () {
+    test('registration screen can be rendered', function () {
+        $response = $this->get(route('register'));
 
-    $response->assertStatus(200);
-});
+        $response->assertStatus(200);
+    });
 
-test('new users can register', function () {
-    $response = $this->post(route('register.store'), [
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
-    ]);
+    test('new users can register', function () {
+        $response = $this->post(route('register.store'), [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+        $this->assertAuthenticated();
+        $response->assertRedirect(route('dashboard', absolute: false));
+    });
 });
